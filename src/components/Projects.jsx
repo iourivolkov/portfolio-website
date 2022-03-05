b12887aca1973a3996b022f598b8fd986b77c2b3
Iouri Volkov <volk.iouri@gmail.com>
2022-03-05 17:46:34 -0500

    add text decoration effects to project links and navigation links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,12 +6,12 @@ const Projects = ({img, name, description, technology, links}) => {
   return (
     <div className="container">
       <h1 className="recent-work">BROWSE PROJECTS</h1>
-      <div className="past-project-links">
-        <h2>Secret Lurry Society</h2>
-        <h2>Scheduler</h2>
-        <h2>Light Mart</h2>
-        <h2>Tweeter</h2>
-        <h2>Tiny App</h2>
+      <div>
+        <h2 className="past-project-links">Secret Lurry Society</h2>
+        <h2 className="past-project-links">Scheduler</h2>
+        <h2 className="past-project-links">Light Mart</h2>
+        <h2 className="past-project-links">Tweeter</h2>
+        <h2 className="past-project-links">Tiny App</h2>
       </div>
       
     </div>
