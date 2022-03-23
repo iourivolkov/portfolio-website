6731170ed372ec9ebf14fd7c579debf0adde4c6b
Iouri Volkov <volk.iouri@gmail.com>
2022-03-22 23:15:52 -0400

    Update about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,10 +7,11 @@ const About = () => {
         <h1 className="about-heading">ABOUT</h1>
         <p className="about-desc">
           <i>
-            "Iouri is a <b>Full-Stack Web Developer</b> and self-taught
-            <b> UX designer </b>
+            "Iouri is a <b>Full-Stack Web Developer</b>, as well as a
+            self-taught
+            <b> UX designer </b> and <b>Photographer </b>
             with a passion for creating intuitive user experiences and
-            introducing new users to <b>Web 3.0</b>."
+            introducing new users to <b>Blockchain technology</b>."
           </i>
         </p>
       </div>
