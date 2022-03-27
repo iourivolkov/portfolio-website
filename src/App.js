33567dddd3998d0d6a9b18770ab9c519bcaf482a
Iouri Volkov <volk.iouri@gmail.com>
2022-03-26 23:08:48 -0400

    Add bouncing ball effect

--- src/App.js
+++ src/App.js
@@ -6,25 +6,34 @@ import Footer from "./components/Footer";
 import Photos from "./components/Photos";
 import About from "./components/About";
 
-import Toronto from "./assets/toronto.png";
-
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as CANNON from "cannon-es";
 
 function App() {
   const mountRef = useRef(null);
   useEffect(() => {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
-      75,
+      30,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
-    const renderer = new THREE.WebGLRenderer();
+
+    const ambientLight = new THREE.AmbientLight(0x333333);
+    scene.add(ambientLight);
+
+    // const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    // scene.add(directionalLight);
+    // directionalLight.position.set(0,50,0);
+    // directionalLight.castShadow = true;
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
 
     renderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer.shadowMap.enabled = true;
 
     // scene is appended directly to the document body
     mountRef.current.appendChild(renderer.domElement);
@@ -49,11 +58,12 @@ function App() {
     directionalLight.position.y = 0;
     directionalLight.position.z = 1;
     scene.add(directionalLight);
+    directionalLight.castShadow = true;
 
-    const directional_Light = new THREE.DirectionalLight(0xffffff);
-    directional_Light.position.y = 0;
-    directional_Light.position.z = 1;
-    scene.add(directional_Light);
+    // const directional_Light = new THREE.DirectionalLight(0xffffff);
+    // directional_Light.position.y = 0;
+    // directional_Light.position.z = 1;
+    // scene.add(directional_Light);
 
     camera.position.z = 5;
 
@@ -76,17 +86,66 @@ function App() {
       raycaster.ray.intersectPlane(plane, intersectionPoint);
     });
 
+    const meshes = [];
+    const bodies = [];
+
     window.addEventListener("click", (e) => {
       const sphereGeo = new THREE.SphereGeometry(0.125, 30, 30);
       const sphereMat = new THREE.MeshStandardMaterial({
-        color: 0xffea00,
+        color: Math.random() * 0xffffff,
         metalness: 0,
         roughness: 0,
       });
       const sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);
       scene.add(sphereMesh);
-      sphereMesh.position.copy(intersectionPoint);
+      sphereMesh.castShadow = true;
+      // sphereMesh.position.copy(intersectionPoint);
+
+      const spherePhysMat = new CANNON.Material();
+      const sphereBody = new CANNON.Body({
+        mass: 0.3,
+        shape: new CANNON.Sphere(0.125),
+        position: new CANNON.Vec3(
+          intersectionPoint.x,
+          intersectionPoint.y,
+          intersectionPoint.z
+        ),
+        material: spherePhysMat,
+      });
+      world.addBody(sphereBody);
+
+      const planeSphereContactMat = new CANNON.ContactMaterial(
+        planePhysMat,
+        spherePhysMat,
+        { restitution: 0.3 }
+      );
+
+      world.addContactMaterial(planeSphereContactMat);
+
+      meshes.push(sphereMesh);
+      bodies.push(sphereBody);
     });
+
+    const world = new CANNON.World({ gravity: new CANNON.Vec3(0, -9.81, 0) });
+    const planeGeo = new THREE.PlaneGeometry(10, 10);
+    const planeMat = new THREE.MeshStandardMaterial({
+      color: "#0000ffff",
+      side: THREE.DoubleSide,
+    });
+    const planeMesh = new THREE.Mesh(planeGeo, planeMat);
+    scene.add(planeMesh);
+    planeMesh.receiveShadow = true;
+
+    const planePhysMat = new CANNON.Material();
+
+    const planeBody = new CANNON.Body({
+      type: CANNON.Body.STATIC,
+      shape: new CANNON.Box(new CANNON.Vec3(5, 5, 0.001)),
+      material: planePhysMat,
+    });
+
+    planeBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+    world.addBody(planeBody);
 
     //Load background texture
     // const loader = new THREE.TextureLoader();
@@ -94,9 +153,23 @@ function App() {
     //   scene.background = texture;
     // });
 
-    const orbit = new OrbitControls(camera, renderer.domElement);
+    // Add Orbit Controls to canvas (imported at top of doc)
+    // const orbit = new OrbitControls(camera, renderer.domElement);
+    // bodies.push(sphereBody);
+
+    const timestep = 1 / 60;
 
     const animate = function () {
+      world.step(timestep);
+
+      planeMesh.position.copy(planeBody.position);
+      planeMesh.quaternion.copy(planeBody.quaternion);
+
+      for (let i = 0; i < meshes.length; i++) {
+        meshes[i].position.copy(bodies[i].position);
+        meshes[i].quaternion.copy(bodies[i].quaternion);
+      }
+
       requestAnimationFrame(animate);
       // cube.rotation.x += 0.01;
       // cube.rotation.y += 0.01;
@@ -113,8 +186,10 @@ function App() {
     window.addEventListener("resize", onWindowResize, false);
 
     animate();
+
     return () => mountRef.current.removeChild(renderer.domElement);
   }, []);
+
   return (
     <div className="App">
       <Navigation />
